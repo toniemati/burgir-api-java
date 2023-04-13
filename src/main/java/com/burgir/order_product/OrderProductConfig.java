2c5f75001e84faecb1070efd38ebdca06fde484a
toniemati <matikas2000@gmail.com>
2023-04-13 21:33:07 +0200

    order product seeder

--- src/main/java/com/burgir/order_product/OrderProductConfig.java
+++ src/main/java/com/burgir/order_product/OrderProductConfig.java
@@ -1,7 +1,12 @@
 package com.burgir.order_product;
 
+import java.util.List;
+
+import com.burgir.orders.Order;
 import com.burgir.orders.OrderRepository;
+import com.burgir.product.Product;
 import com.burgir.product.ProductRepository;
+import com.github.javafaker.Faker;
 
 public class OrderProductConfig {
 
@@ -10,7 +15,20 @@ public class OrderProductConfig {
       OrderRepository orderRepository,
       ProductRepository productRepository) {
 
-    orderProductRepository.addProduct(1l, 1l, 5l);
+    Faker faker = new Faker();
+    Long id = 1l;
+    List<Order> orders = orderRepository.findAll();
+    List<Product> products = productRepository.findAll();
+
+    for (Order o : orders) {
+      Long numOfProducts = faker.number().numberBetween(1l, 6l);
+
+      for (int i = 0; i < numOfProducts; i++) {
+        Product product = products.get(faker.number().numberBetween(1, products.size()));
+
+        orderProductRepository.addProduct(id++, o.getId(), product.getId());
+      }
+    }
   }
 
 }
